3bb55e0226ac02e122008d61d530520eb366e52a
David Gold <david.gold.a@gmail.com>
2024-02-26 19:03:24 -0500

    Custom deser is 10x faster even w/o static typing. Nice.

--- crates/redesmyn/src/schema.rs
+++ crates/redesmyn/src/schema.rs
@@ -1,82 +1,86 @@
-use std::{any::Any, collections::HashMap};
+use std::iter::repeat;
 
-use arrow::array::Array;
 use polars::datatypes::{AnyValue, DataType};
-// use polars::prelude::Schema;
-use pyo3::prelude::*;
 use serde::{
     de::{DeserializeSeed, MapAccess, Visitor},
-    ser::Error,
-    Deserialize, Deserializer,
+    Deserializer,
 };
 use serde_json::Value;
 
 type ScalarType<'a> = AnyValue<'a>;
 type ColumnType<'a> = Vec<ScalarType<'a>>;
 
-
 fn parse<'a>(dtype: &DataType, v: Value) -> ScalarType<'a> {
     match dtype {
+        DataType::Int64 => v.as_i64().map(|v| AnyValue::Int64(v)).unwrap_or(AnyValue::Null),
         DataType::Float64 => v.as_f64().map(|v| AnyValue::Float64(v)).unwrap_or(AnyValue::Null),
+        DataType::String => {
+            v.as_str().map(move |v| AnyValue::StringOwned(v.into())).unwrap_or(AnyValue::Null)
+        }
         _ => todo!(),
     }
 }
 
 #[derive(Clone, Debug)]
-pub struct Field {
-    pub name: String,
+pub struct Field<'a> {
+    pub name: &'a str,
     pub data_type: DataType,
+    pub index: u8,
 }
 
-#[derive(Clone)]
-pub struct Schema {
-    pub fields: Vec<Field>,
+#[derive(Clone, Debug)]
+pub struct Schema<'a> {
+    pub fields: Vec<Field<'a>>,
+    pub columns: Vec<ColumnType<'a>>,
 }
 
-impl Schema {
-    pub fn columns<'a>(&self) -> HashMap<String, (DataType, ColumnType<'a>)> {
-        let mut columns =
-            HashMap::<String, (DataType, ColumnType<'a>)>::with_capacity((&self).fields.len());
-        for field in self.fields.iter() {
-            columns.insert(field.name.clone(), (field.data_type.clone(), ColumnType::<'a>::new()));
+impl<'a> Schema<'a> {
+    pub fn new(fields: Vec<Field<'a>>, capacity: Option<usize>) -> Schema<'a> {
+        let column_factory: Box<dyn FnOnce() -> Vec<AnyValue<'a>>> = match capacity {
+            Some(capacity) => Box::new(move || ColumnType::<'a>::with_capacity(capacity)),
+            None => Box::new(|| ColumnType::<'a>::new())
+        };
+        let n_fields = &fields.len();
+        Schema {
+            fields,
+            columns: repeat(column_factory()).take(*n_fields).collect(),
         }
-        columns
     }
 }
 
+struct SchemaVisitor<'a>(Schema<'a>);
 
-#[derive(Debug)]
-pub struct Columns<'a>(pub HashMap<String, (DataType, ColumnType<'a>)>);
+impl<'de, 'a> Visitor<'de> for SchemaVisitor<'a> {
+    type Value = Schema<'a>;
 
-impl<'de, 'a> DeserializeSeed<'de> for Columns<'a> {
-    type Value = Self;
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a mapping of fields to values")
+    }
 
-    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    fn visit_map<M>(mut self, mut visited: M) -> Result<Schema<'a>, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        while let Some((key, value)) = visited.next_entry::<&str, Value>()? {
+            let Ok(index) = &self.0.fields[..].binary_search_by_key(&key, |f| f.name) else {
+                continue;
+            };
+            self.0
+                .columns
+                .get_mut(*index)
+                .map(|col| col.push(parse(&self.0.fields[*index].data_type, value)));
+        }
+        Ok(self.0)
+    }
+}
+
+impl<'de, 'a> DeserializeSeed<'de> for Schema<'a> {
+    type Value = Schema<'a>;
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        pub struct ColumnsVisitor<'a>(Columns<'a>);
-
-        impl<'de, 'a> Visitor<'de> for ColumnsVisitor<'a> {
-            type Value = Columns<'a>;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("a mapping of fields to values")
-            }
-
-            fn visit_map<M>(mut self, mut visited: M) -> Result<Self::Value, M::Error>
-            where
-                M: MapAccess<'de>,
-            {
-                while let Some((key, value)) = visited.next_entry::<String, Value>()? {
-                    self.0.0.get_mut(&key).map(|(dtype, col)| {
-                        col.push(parse(dtype, value))
-                    });
-                }
-                Ok(self.0)
-            }
-        }
-
-        deserializer.deserialize_map(ColumnsVisitor(self))
+        deserializer.deserialize_map(SchemaVisitor(self))
     }
 }
--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,10 +1,11 @@
-use std::collections::HashMap;
+use std::iter::repeat;
+use std::{collections::HashMap, time::Instant};
 
 use polars::prelude::*;
-use redesmyn::schema;
 use redesmyn::{
     error::ServiceError,
     predictions::{BatchPredictor, Configurable},
+    schema::{self},
     server::{Serve, Server},
 };
 // use redesmyn_macros::Schema;
@@ -31,30 +32,108 @@ fn main() -> Result<(), ServiceError> {
 
     let json = r#"
         {
-            "a": 1,
-            "b": null
+            "a": 1.0,
+            "b": null,
+            "c": "Foo",
+            "d": 13321312312
         }
     "#;
 
-    let schema = schema::Schema {
-        fields: vec![
-            schema::Field {
-                name: "a".to_string(),
-                data_type: DataType::Float64,
-            },
-            schema::Field {
-                name: "b".to_string(),
-                data_type: DataType::Float64,
-            },
-        ],
+    let n_records: usize = 100_000;
+    fn get_schema<'a>(n_records: Option<usize>) -> schema::Schema<'a> {
+        schema::Schema::new(
+            vec![
+                schema::Field {
+                    name: "a",
+                    data_type: DataType::Float64,
+                    index: 0,
+                },
+                schema::Field {
+                    name: "b",
+                    data_type: DataType::Float64,
+                    index: 1,
+                },
+                schema::Field {
+                    name: "c",
+                    data_type: DataType::String,
+                    index: 2,
+                },
+                schema::Field {
+                    name: "d",
+                    data_type: DataType::Int64,
+                    index: 3,
+                },
+            ],
+            n_records,
+        )
+    }
+
+    let records = repeat(json).take(n_records).collect::<Vec<_>>();
+
+    let schema: schema::Schema = records.into_iter().fold(
+        Ok(get_schema(None)),
+        |schema: Result<schema::Schema, _>, record| {
+            let mut de = Deserializer::from_str(record);
+            schema?.deserialize(&mut de)
+        },
+    )?;
+
+    let start = Instant::now();
+
+    let series = schema
+        .columns
+        .iter()
+        .zip(schema.fields)
+        .filter_map(|(col, field)| {
+            match Series::from_any_values_and_dtype(field.name, &col, &field.data_type, true) {
+                Ok(series) => Some(series),
+                Err(err) => {
+                    println!("Error; {err}");
+                    None
+                }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let df = DataFrame::new(series)?;
+    println!("{:#?}", start.elapsed());
+    println!("{df}");
+
+    #[derive(Deserialize)]
+    struct ToyRecord {
+        a: f64,
+        b: Option<f64>,
+        c: String,
+        d: i64,
+    }
+
+    let record = ToyRecord {
+        a: 1.0,
+        b: None,
+        c: "Foo".to_string(),
+        d: 13321312312,
     };
 
-    let mut de = Deserializer::from_str(json);
-    let columns: schema::Columns = schema::Columns(schema.columns()).deserialize(&mut de)?;
-    let series =
-    Series::from_any_values_and_dtype("a", &columns.0.get("a").unwrap().1[..], &columns.0.get("a").unwrap().0, true)?;
-    println!("{:#?}", series.head(Some(1)));
-    // columns.0.iter().
-    // print!("{:#?}", columns);
+    let start_2 = Instant::now();
+    let (a, b, c, d) = repeat(json).take(n_records).map(|r| serde_json::from_str(r).unwrap()).fold(
+        (Vec::<f64>::new(), Vec::<Option<f64>>::new(), Vec::<String>::new(), Vec::<i64>::new()),
+        |(mut a, mut b, mut c, mut d), r: ToyRecord| {
+            a.push(r.a);
+            b.push(r.b);
+            c.push(r.c);
+            d.push(r.d);
+            (a, b, c, d)
+        },
+    );
+    let df = DataFrame::new(vec![
+        Series::new("a", a),
+        Series::new("b", b),
+        Series::new("c", c),
+        Series::new("d", d),
+    ])?;
+
+    println!("{:#?}", start_2.elapsed());
+    println!("{df}");
+
     Ok(())
 }
