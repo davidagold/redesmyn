39998a9bc0e14d8358caef414ff21dc75789bbb2
David Gold <david.gold.a@gmail.com>
2024-04-15 07:53:24 -0700

    Work on Python artifacts functionality

--- py-redesmyn/redesmyn/artifacts.py
+++ py-redesmyn/redesmyn/artifacts.py
@@ -1,7 +1,35 @@
-import abc
-from typing import Dict, Generic, Iterable, List, Optional, TypeVar, cast
+from abc import abstractmethod
+import re
+from string import Template
+from typing import (
+    Callable,
+    ClassVar,
+    Dict,
+    Generic,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+    get_args,
+)
+from more_itertools import filter_map, only
 
-from more_itertools import last
+from pydantic import BaseModel, ConfigDict, create_model
+from pydantic.fields import FieldInfo
+
+
+def ensure_trailing_slash(path_component: str) -> str:
+    return re.sub(r"^(.*)(?!/)$", r"\1/", path_component)
+
+
+class PathTemplate(Template):
+    delimiter = ""
+    idpattern = r"(?a: )"
+    braceidpattern = r"(?a:[_a-z][_a-z0-9]*)"
 
 
 class EndpointPath:
@@ -31,56 +59,146 @@ class EndpointPath:
         self.query = self.query | query_params
 
 
-class ArtifactSpecMeta(abc.ABCMeta):
+# TODO: Write ordering protocol and use for bound
+L = TypeVar("L", bound=Union[int, str])
+
+
+class LatestKey(Generic[L]):
+    def __init__(self, key: L) -> None:
+        self._key = key
+
+    def key(self) -> L:
+        return self._key
+
+
+class ArtifactSpec(BaseModel, Generic[L]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    _cache_path: ClassVar[Optional[PathTemplate]]
+    _latest_key: ClassVar[Optional[str]]
+
+    @classmethod
+    def latest_key(cls) -> Optional[str]:
+        return cls._latest_key
+
+    def has_latest(self) -> bool:
+        latest_key = self.latest_key()
+        return latest_key is not None and getattr(self, latest_key) is None
+
+    @classmethod
+    def cache_path(cls) -> Optional[PathTemplate]:
+        return cls._cache_path
+
+    @classmethod
+    def from_path(cls, path: str | PathTemplate, spec_name: str) -> Type["ArtifactSpec"]:
+        param_ids = (
+            path.get_identifiers()
+            if isinstance(path, PathTemplate)
+            else PathTemplate(path).get_identifiers()
+        )
+        params = {k: (Optional[str], None) for k in param_ids}
+        return create_model(spec_name, __base__=(ArtifactSpec,), **params)
+        # return ArtifactSpec(**{param: str for param in path.get_identifiers()})
+
+    @staticmethod
+    def generate_subclass(base: Type[BaseModel]) -> Type["ArtifactSpec"]:
+        # def is_latest_key(key_field_tuple: Tuple[str, FieldInfo]) -> Optional[str]:
+        def is_latest_key(key_field_tuple: Tuple[str, FieldInfo]) -> Optional[str]:
+            key, field = key_field_tuple
+            key_cls = only(
+                arg
+                for arg in get_args(field.rebuild_annotation())
+                if isinstance(arg, Type) and issubclass(arg, LatestKey)
+            )
+            if key_cls is not None:
+                return key
+            else:
+                return None
+
+        latest_key = only(filter_map(is_latest_key, base.model_fields.items()))
+        spec: Type[ArtifactSpec] = create_model(f"{base.__name__}Spec", __base__=(base, ArtifactSpec))
+        spec._latest_key = latest_key
+        return spec
+
     def __truediv__(self, other: str) -> EndpointPath:
-        # return "/".join([cast(ArtifactSpec, self).as_url_params(), other])
-        return cast(ArtifactSpec, self).as_url_params() / other
-
-    def __rtruediv__(self, other: str) -> EndpointPath:
-        # return "/".join([other, cast(ArtifactSpec, self).as_url_params()])
-        return other / cast(ArtifactSpec, self).as_url_params()
-
-
-class ArtifactSpec(metaclass=ArtifactSpecMeta):
-    def __init__(self, **kwargs: type) -> None:
-        self.fields = list(kwargs.keys())
-
-    def as_url_params(self) -> EndpointPath:
-        # return "/".join(f"{{{k}}}" for k in get_type_hints(cls))
-        return EndpointPath(parts=[f"{{{k}}}" for k in self.fields])
-
-    def __truediv__(self, other: str) -> EndpointPath:
-        # return "/".join([cast(ArtifactSpec, self).as_url_params(), other])
         return self.as_url_params() / other
 
     def __rtruediv__(self, other: str) -> EndpointPath:
-        # return "/".join([other, cast(ArtifactSpec, self).as_url_params()])
         return other / self.as_url_params()
 
+    def as_url_params(self) -> EndpointPath:
+        return EndpointPath(parts=[f"{{{f}}}" for f in self.model_fields])
 
-T = TypeVar("T")
-class ModelCache(Generic[T]):
+
+def spec(
+    cache_path: Optional[str] = None, latest_key: Optional[str] = None
+) -> Callable[[Type[BaseModel]], Type[ArtifactSpec]]:
+    def wrapper(cls: Type[BaseModel]) -> Type[ArtifactSpec]:
+        spec = ArtifactSpec.generate_subclass(cls)
+        spec._latest_key = spec._latest_key or latest_key
+        if cache_path:
+            spec._cache_path = PathTemplate(cache_path)
+            if errors := set(spec._cache_path.get_identifiers()) ^ set(spec.model_fields):
+                msg = f"Mismatch between path template and spec fields: {errors}"
+                raise ValueError(msg)
+
+        return spec
+
+    return wrapper
+
+
+M = TypeVar("M", covariant=True)
+
+
+class ArtifactsClient(Generic[M]):
+    @abstractmethod
+    def _list(self, path: str) -> Iterable[str]:
+        pass
+
+    @abstractmethod
+    def _get(self, path: str) -> M:
+        pass
+
+    def list(self, path: PathTemplate, **kwargs) -> Iterable[str]:
+        return self._list(path.substitute(**kwargs))
+
+    def get(self, path: PathTemplate, **kwargs) -> M:
+        return self._get(path.substitute(**kwargs))
+
+
+T = TypeVar("T", bound=Type[ArtifactSpec])
+
+
+class ModelCache(Generic[T, M]):
     def __init__(
         self,
-        artifact_spec: ArtifactSpec,
-        storage: str,
+        client: ArtifactsClient[M],
+        spec: T,
+        path: Optional[str] = None,
         latest_key: Optional[str] = None,
     ) -> None:
-        self.artifact_spec = artifact_spec
-        self.storage = storage
-        self.latest_key: str = latest_key or last(self.artifact_spec.fields)
+        if (path is None) ^ (spec.cache_path() is not None):
+            raise ValueError("Precisely one of `path` or `spec.cache_path()` must not be `None`")
+        if path and not path.endswith("/"):
+            raise ValueError(f"Path must end with '/' (received '{path}').")
+        if not issubclass(spec, ArtifactSpec):
+            raise ValueError()
 
-    def get(self, **kwargs) -> T:
-        # assert all(isinstance(v, self.artifact_spec))
-        errors = set(self.artifact_spec.fields) ^ set(kwargs)
-        assert len(errors) == 0, f"{errors=}"
-        return
-    
-    def get_latest(self, **kwargs) -> T:
-        # assert all(isinstance(v, self.artifact_spec))
-        errors = set([k for k in self.artifact_spec.fields if k != self.latest_key]) ^ set(kwargs)
-        assert len(errors) == 0, f"{errors=}"
-        return
+        self._client = client
+        self._path = cast(PathTemplate, PathTemplate(path) if path is not None else spec.cache_path())
+        self._spec = spec
+        self._latest_key = latest_key
+
+    def get(self, **kwargs) -> M:
+        self._spec.model_validate(kwargs)
+        return self._client.get(self._path, **kwargs)
+
+    def get_latest(self, **kwargs) -> M:
+        params = self._spec.model_validate(kwargs)
+        if (latest_key := params.latest_key()) is not None:
+            v = getattr(params, latest_key)
+            if (v := getattr(params, latest_key)) is not None:
+                msg = f"Cannot fetch latest when `latest_key='{latest_key}' is set to {v}`"
+                raise ValueError(msg)
 
     def referesh(self, **kwargs):
         pass
---
+++ py-redesmyn/tests/test_artifacts.py
@@ -0,0 +1,124 @@
+from enum import Enum
+from typing import Annotated, Any, Optional, Self, cast
+from annotated_types import Ge
+from more_itertools import first, one
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    Field,
+    ValidationError,
+    ValidationInfo,
+    field_validator,
+)
+import pytest
+
+from redesmyn import artifacts as afs
+
+
+class DummyClient(afs.ArtifactsClient):
+    def __init__(self) -> None:
+        pass
+
+    def _list(self, path: str) -> afs.Iterable[str]:
+        pass
+
+    def _get(self, path: str) -> Any:
+        print(f"{path=}")
+
+
+class TestEndpointPath:
+    def test_with_query_params(self):
+        assert False
+
+        # path.with_query_params(use_feature=bool)
+        # assert (
+        #     path.__repr__()
+        #     == "EndpointPath('/predictions/{source}/{region}/{id}?use_feature=<bool>')"
+        # )
+
+
+class FromString(Enum):
+    @classmethod
+    def from_string(cls, v: str) -> Self:
+        return one(variant for variant in cls if variant.value == v)
+
+
+class Iso3166_1(FromString):
+    US = "US"
+    GB = "GB"
+
+
+class Iso3166_2(FromString):
+    US_CA = "US-CA"
+    US_NY = "US-NY"
+    GB_ENG = "GB-ENG"
+    GB_NIR = "GB-NIR"
+
+    def is_subdivision(self, of: Iso3166_1) -> bool:
+        return first(self.value.split("-")) == of.value
+
+
+@afs.spec(cache_path="s3://model-bucket/{iso3166_1}/{iso3166_2}/{id}")
+class DummyArtifact(BaseModel):
+    iso3166_1: Annotated[Iso3166_1, BeforeValidator(Iso3166_1.from_string)]
+    iso3166_2: Iso3166_2
+    id: Annotated[int, Ge(0), afs.LatestKey] = Field(default=None)
+
+    @field_validator("iso3166_2", mode="before")
+    @classmethod
+    def validate_iso3166_2(cls, v: str, info: ValidationInfo) -> Iso3166_2:
+        iso3166_1: Optional[Iso3166_1] = info.data.get("iso3166_1")
+        iso3166_2 = Iso3166_2.from_string(v)
+        if iso3166_1 is None or not iso3166_2.is_subdivision(of=iso3166_1):
+            raise ValueError(f"'{iso3166_2} is not a subdivision of {iso3166_1}")
+
+        return iso3166_2
+
+
+class TestArtifactSpec:
+    def test(self):
+        path = "s3://model-bucket/{iso3166_1}/{iso3166_2}/{id}"
+        assert DummyArtifact.cache_path() is not None
+        assert (
+            cast(afs.PathTemplate, DummyArtifact.cache_path()).template
+            == afs.PathTemplate(path).template
+        )
+
+
+class TestModelCache:
+    client = DummyClient()
+    path = "s3://model-bucket/{iso3166_1}/{iso3166_2}/{id}/"
+
+    def test_get(self):
+        cache = afs.ModelCache(client=self.client, spec=DummyArtifact, latest_key="id")
+        cache.get(iso3166_1="US", iso3166_2="US-CA", id=123)
+
+    def test_get_latest(self):
+        print(f"{DummyArtifact.latest_key()=}")
+        cache = afs.ModelCache(client=self.client, spec=DummyArtifact, latest_key="id")
+        cache.get_latest(iso3166_1="US", iso3166_2="US-CA")
+        with pytest.raises(Exception):
+            cache.get_latest(iso3166_1="US", iso3166_2="US-CA", id=123)
+
+    def test_invalid_iso3166_1(self):
+        cache = afs.ModelCache(client=self.client, spec=DummyArtifact, latest_key="id")
+        with pytest.raises(ValidationError):
+            cache.get_latest(iso3166_1="FR", iso3166_2="FR-17", id=123)
+
+    def test_invalid_iso3166_2(self):
+        cache = afs.ModelCache(client=self.client, spec=DummyArtifact, latest_key="id")
+        with pytest.raises(ValidationError) as e:
+            cache.get_latest(iso3166_1="US", iso3166_2="GB-ENG", id=123)
+
+        error = one(e.value.errors())
+        assert error["msg"] == "Value error, 'Iso3166_2.GB_ENG is not a subdivision of Iso3166_1.US"
+
+    def test_path_mismatch(self):
+        with pytest.raises(ValueError) as e:
+
+            @afs.spec("s3://model-bucket/{iso3166_1}/{iso3166_2}/{id}/")
+            class MismatchedSpec(BaseModel):
+                iso3166_1: Annotated[Iso3166_1, BeforeValidator(Iso3166_1.from_string)]
+                iso3166_2: Annotated[Iso3166_1, BeforeValidator(Iso3166_2.from_string)]
+
+        assert e.exconly() == "ValueError: Mismatch between path template and spec fields: {'id'}"
