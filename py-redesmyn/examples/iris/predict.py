94e72e73033361b351924d5db30c366987edc87e
David Gold <david.gold.a@gmail.com>
2024-05-30 21:55:17 -0700

    Include iris example

---
+++ py-redesmyn/examples/iris/predict.py
@@ -0,0 +1,53 @@
+import asyncio
+import json
+import time
+from typing import Dict
+
+import aiohttp
+import polars as pl
+
+from common import load_irises
+from train import Input
+
+
+async def send_post_request(
+    task_id: int,
+    session: aiohttp.ClientSession,
+    url: str,
+    data: Dict,
+    records_by_task_id: Dict[int, Dict],
+):
+    start = time.perf_counter_ns()
+    async with session.post(url, json=[json.dumps(data)]) as response:
+        message = await response.text()
+        timing_ms = (time.perf_counter_ns() - start) / 1e9
+        record = json.loads(message)
+        record_request = {"timing": timing_ms, **record}
+        records_by_task_id[task_id] = record_request
+
+
+async def main():
+    url = "http://localhost:8080/predictions/iris/v0.1.0"
+    records_by_task_id: Dict[int, Dict] = {}
+    irises = load_irises()
+
+    async with aiohttp.ClientSession() as session:
+        async with asyncio.TaskGroup() as tg:
+            for idx, row in enumerate(irises.iter_rows()):
+                data = dict(zip(Input.field_names(), row))
+                coro = send_post_request(
+                    task_id=idx,
+                    session=session,
+                    url=url,
+                    data=data,
+                    records_by_task_id=records_by_task_id,
+                )
+                tg.create_task(coro)
+
+    df = pl.DataFrame(list(records_by_task_id.values()))
+    print(df.describe())
+    print(df.head())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
