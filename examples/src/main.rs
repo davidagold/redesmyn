2d659be2fb0da92c216f9056ff7893216729d2f6
David Gold <david.gold.a@gmail.com>
2024-06-13 10:37:35 -0700

    Include model cache functionality in example

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,11 +1,15 @@
+use cron;
 use polars::datatypes::DataType;
+use pyo3::{types::PyFunction, Py, Python};
 use redesmyn::{
+    cache::{ArtifactsClient, Cache, FsClient, Schedule},
     error::ServiceError,
     handler::PySpec,
     predictions::BatchPredictor,
     schema::{Relation, Schema},
     server::Server,
 };
+use std::str::FromStr;
 
 // #[derive(Debug, Deserialize, Relation)]
 // pub struct ToyRecord {
@@ -22,10 +26,28 @@ async fn main() -> Result<(), ServiceError> {
     schema.add_field("b", DataType::Float64);
 
     // let service = BatchPredictor::<String, ToyRecord>::new(schema)
+    let load_model: Py<PyFunction> = Python::with_gil(|py| {
+        py.import("mlflow")?
+            .getattr("sklearn")?
+            .getattr("load_model")?
+            .extract::<Py<PyFunction>>()
+            .map_err(|err| ServiceError::from(err.to_string()))
+    })?;
 
-    let service = BatchPredictor::<String, Schema>::builder()
+    let afs_client = ArtifactsClient::FsClient { client: FsClient::default(), load_model };
+    let cache = Cache::new(
+        afs_client,
+        Some(64),
+        Some(Schedule::Cron(
+            cron::Schedule::from_str("0 * * * * * *")
+                .map_err(|err| ServiceError::from(err.to_string()))?,
+        )),
+    );
+
+    let endpoint = BatchPredictor::<String, Schema>::builder()
         .schema(schema)
-        .path("/predictions/{model_name}/{model_version}")
+        .path("/predictions/{model_version}")
+        .cache(cache)
         .batch_max_size(100)
         .batch_max_delay_ms(5)
         .handler_config(PySpec::new().module("tests.test_server").method("handler").into())
@@ -33,7 +55,7 @@ async fn main() -> Result<(), ServiceError> {
 
     let mut server = Server::default();
     // server.log_config(LogConfig::Stdout);
-    server.register(service);
+    server.register(endpoint);
     server.push_pythonpath("./py-redesmyn");
     server.serve()?.await?;
 
