e061e71e119fa7b0a603ed61ea16d606369f8b46
David Gold <david.gold.a@gmail.com>
2024-07-27 10:00:31 -0700

    Create redesmyn.logging module, move documentation

--- py-redesmyn/redesmyn/py_redesmyn.pyi
+++ py-redesmyn/redesmyn/py_redesmyn.pyi
@@ -49,15 +49,7 @@ class Cache:
 
 class LogConfig:
     def __new__(cls, path: Path, emf_path: Optional[Path] = None) -> Self: ...
-    """`LogConfig` configures logging output for the application.
-
-    Redesmyn applications write to two logging output files: a primary logging output file,
-    specified via the `path` parameter, and an optional Amazon Web Services (AWS)
-    Embedded Metrics Format (EMF) output file, specified via the `emf_path` parameter.
-    """
-
     def init(self): ...
-    """Initialize logging for the present application using this logging configuration."""
 
 class ServerHandle:
     def stop(self, graceful: Optional[bool]) -> Future: ...
