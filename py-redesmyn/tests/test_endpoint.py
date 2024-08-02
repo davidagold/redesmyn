6b23f7e0dd51c61dab8715091a0f39ee39ccc46c
David Gold <david.gold.a@gmail.com>
2024-08-01 18:00:31 -0700

    Stub of test for non-parametrized handlers

--- py-redesmyn/tests/test_endpoint.py
+++ py-redesmyn/tests/test_endpoint.py
@@ -16,7 +16,7 @@ from redesmyn.py_redesmyn import LogConfig
 from redesmyn.service import Server, endpoint
 from sklearn.utils.discovery import itemgetter
 
-from tests.fixtures.iris_model import get_handle
+from tests.fixtures.iris_model import SepalLengthPredictor, get_handle
 
 PROJECT_DIR = Path(__file__).parent.parent
 
@@ -74,8 +74,8 @@ async def serve_and_predict(
             )
 
 
-class TestServer:
-    @mock.patch.dict(os.environ, {"RUST_LOG": "info"})
+class TestEndpoint:
+    @mock.patch.dict(os.environ, {"RUST_LOG": "debug"})
     def test_validates_artifact_params(self, irises: pl.DataFrame):
         dir_logs = Path(__file__).parent / "logs"
         dir_logs.mkdir(exist_ok=True)
@@ -92,3 +92,32 @@ class TestServer:
         assert response_by_run_id["invalid_run_id"]["body"].startswith(
             "Python Error: ValidationError"
         )
+
+    @mock.patch.dict(os.environ, {"RUST_LOG": "debug"})
+    def test_unparametrized_handler(self, irises: pl.DataFrame):
+        dir_logs = Path(__file__).parent / "logs"
+        dir_logs.mkdir(exist_ok=True)
+        with tempfile.NamedTemporaryFile(dir=dir_logs, delete=False) as f:
+            LogConfig(path=Path(f.name)).init()
+
+        model = SepalLengthPredictor(
+            model_uri=(
+                PROJECT_DIR
+                / "examples/iris/models/mlflow/iris/903683212157180428/000449a650df4e36844626e647d15664/artifacts/model"
+            ).as_posix()
+        )
+
+        @endpoint(path="/predictions/iris")
+        def handle(records_df: pl.DataFrame) -> pl.DataFrame:
+            return model.include_predictions(records_df=records_df)
+
+        # server = Server()
+        # server.register(get_handle(validate_artifact_params=True))
+        # response_by_run_id = {}
+        # coro = serve_and_predict(server=server, irises=irises, response_by_run_id=response_by_run_id)
+        # asyncio.run(coro)
+        # assert response_by_run_id["903683212157180428"]["http_status_code"] == 200
+        # assert response_by_run_id["invalid_run_id"]["http_status_code"] == 422
+        # assert response_by_run_id["invalid_run_id"]["body"].startswith(
+        #     "Python Error: ValidationError"
+        # )
