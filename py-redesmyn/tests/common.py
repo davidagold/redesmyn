fc57182d290ff54ff3a164ba6ebdae9ba655d67c
David Gold <david.gold.a@gmail.com>
2024-08-05 14:47:22 -0700

    Try retrying

--- py-redesmyn/tests/common.py
+++ py-redesmyn/tests/common.py
@@ -1,7 +1,8 @@
 import asyncio
 import json
+import logging
 from pathlib import Path
-from typing import Callable, Coroutine, Dict, List, Optional
+from typing import Callable, Coroutine, Dict, List, Optional, Type
 
 import aiohttp
 import polars as pl
@@ -11,6 +12,27 @@ from redesmyn import service as svc
 DIR_TESTS = Path(__file__).parent
 DIR_PYREDESMYN = DIR_TESTS.parent
 DIR_PROJECT = DIR_PYREDESMYN.parent
+
+
+async def retry(
+    coro: Coroutine,
+    retryable: List[Type[Exception]],
+    exp_backoff_coef_ms: int = 1000,
+    max_num_attempts: int = 3,
+):
+    num_attempts = 0
+    while num_attempts <= max_num_attempts:
+        num_attempts += 1
+        try:
+            return await coro
+        except Exception as e:
+            if type(e) in retryable:
+                wait_ms = exp_backoff_coef_ms * 2 ** (num_attempts - 1)
+                num_attempts_remaining = max_num_attempts - num_attempts
+                logging.warn(f"Encountered exception {e} while trying {coro}")
+                await asyncio.sleep(wait_ms / 1000)
+            else:
+                raise e
 
 
 async def request_prediction(
@@ -35,6 +57,9 @@ async def serve_and_predict(
     async with aiohttp.ClientSession() as session, asyncio.TaskGroup() as server_tg:
         server_tg.create_task(server.serve())
         async with asyncio.TaskGroup() as inner_tg:
-            [inner_tg.create_task(t) for t in tasks(session=session, data=data)]
+            [
+                inner_tg.create_task(retry(t, retryable=[aiohttp.client.ClientConnectorError]))
+                for t in tasks(session=session, data=data)
+            ]
 
         server_tg.create_task(server.stop())
