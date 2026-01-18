00de0116990964858c9f6b4e6d26e6e5639989f6
David Gold <david.gold.a@gmail.com>
2026-01-18 15:22:17 -0800

    rust: emit startup span in server/daemon

--- rust/crates/redesmyn-daemon/src/main.rs
+++ rust/crates/redesmyn-daemon/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     redesmyn_logging::init();
+
+    let span = redesmyn_logging::redesmyn_info_span!("startup", component = "redesmyn-daemon");
+    redesmyn_logging::span::record_run_id(&span, std::process::id());
+
+    let _guard = span.enter();
+    redesmyn_logging::tracing::info!("starting");
 }
